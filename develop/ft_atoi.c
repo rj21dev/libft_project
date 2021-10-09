6c2a917f0e5224c0d9bc150bd56c1415212580ba
Rennifer Jada <rjada@oa-g5.msk.21-school.ru>
2021-10-09 14:12:32 +0300

    rm

---
+++ develop/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjada <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/07 19:30:31 by rjada             #+#    #+#             */
+/*   Updated: 2021/10/07 22:08:29 by rjada            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_extreme(int res, unsigned long long extreme, int sign)
+{
+	unsigned long long	min;
+	unsigned long long	max;
+
+	min = 9223372036854775808U;
+	max = 9223372036854775807U;
+	if (extreme > max && sign == 1)
+		return (-1);
+	if (extreme > min && sign == -1)
+		return (0);
+	return (res * sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	int					res;
+	unsigned long long	extreme;
+
+	sign = 1;
+	res = 0;
+	extreme = 0;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		++i;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		++i;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		extreme = extreme * 10 + str[i] - '0';
+		++i;
+	}
+	return (ft_extreme(res, extreme, sign));
+}
