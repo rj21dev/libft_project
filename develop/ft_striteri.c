bb4c0a06389e4389ac23dc732b03079b0cef7eaa
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 17:04:40 +0300

    Update ft_striteri.c

--- develop/ft_striteri.c
+++ develop/ft_striteri.c
@@ -10,13 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	if (!s || !f)
+	if (!s)
 		return ;
 	i = 0;
 	while (s[i])
