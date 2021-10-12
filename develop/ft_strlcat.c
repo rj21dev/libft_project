76963edd4cf63c3b4611bb7a78afc0e2d536e9df
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 17:08:29 +0300

    Update ft_strlcat.c

--- develop/ft_strlcat.c
+++ develop/ft_strlcat.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
