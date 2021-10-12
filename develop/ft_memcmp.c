2bb266cb5eb230a87c81ff4b7c03f5b9fe812be2
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 16:55:02 +0300

    Update ft_memcmp.c

--- develop/ft_memcmp.c
+++ develop/ft_memcmp.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
