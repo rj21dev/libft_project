56ce93763b795897ac763f344fb6f924f8d06d00
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 17:10:09 +0300

    Update ft_strlcpy.c

--- develop/ft_strlcpy.c
+++ develop/ft_strlcpy.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
