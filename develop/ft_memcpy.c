765605c6c4582a4879b4b119f572a6407acd090b
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 16:55:37 +0300

    Update ft_memcpy.c

--- develop/ft_memcpy.c
+++ develop/ft_memcpy.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
