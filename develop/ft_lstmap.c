da689bb7ef43e328166d34884bfb585d41c94819
Oleg <66532960+rjada@users.noreply.github.com>
2021-10-12 16:51:38 +0300

    Update ft_lstmap.c

--- develop/ft_lstmap.c
+++ develop/ft_lstmap.c
@@ -18,8 +18,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*last;
 	t_list	*node;
 
-	new_list = NULL;
-	last = NULL;
+	new_list = ((void *)0);
+	last = ((void *)0);
 	while (lst)
 	{
 		node = ft_lstnew((*f)(lst->content));
